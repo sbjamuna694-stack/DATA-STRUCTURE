cdc5e936971531676b1584cb50969436ea13c35f
sbjamuna694-stack <sbjamuna694@gmail.com>
2026-04-16 19:40:15 +0530

    Add files via upload

---
+++ DS lab 9 Implementing single LL.c
@@ -0,0 +1,15 @@
+// implementing singly linked list : insetion,deletion,search
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node*next;
+};
+struct node*createnode(int data){
+struct node*newnode=(struct node*)malloc(sizeof(struct node));
+new node->data=data;
+newnode->next=NULL;
+return new node;
+};
+void insertatbeginning(struct node**head,int data){
+}
